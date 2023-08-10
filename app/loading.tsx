fd6b04700606728aaa76666f6f1af6fd314b77b2
Turfa Auliarachman <me@turfa.id>
2023-08-10 14:26:30 +0700

    move navbar, add loading suspense

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import {Title} from "@tremor/react";
+
+export default function Loading() {
+    return (
+        <main className="p-4 md:p-10 mx-auto max-w-7xl">
+            <Title>Loading...</Title>
+        </main>
+    )
+}
