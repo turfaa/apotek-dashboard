85e7752a085ce6b323ab1e8cd22617a0b2e37c7b
Turfa Auliarachman <me@turfa.id>
2023-11-01 06:33:10 +0700

    fix(tabs): Hide tabs in print mode

--- app/drugs-to-stock-opname/tab-list.tsx
+++ app/drugs-to-stock-opname/tab-list.tsx
@@ -1,10 +1,17 @@
 "use client"
 
+import { usePrintMode } from "@/lib/print-mode"
 import { BookOpenIcon, ShoppingBagIcon } from "@heroicons/react/24/outline"
 import { Tab, TabList } from "@tremor/react"
 
 // DrugsToStockOpnameTabList is created in a different file because it needs to be a Client Component to import the icons, while we want the other parts to be Server Components.
 export default function DrugsToStockOpnameTabList(): React.ReactElement {
+    const { isPrintMode } = usePrintMode()
+
+    if (isPrintMode) {
+        return <></>
+    }
+
     return (
         <TabList>
             <Tab icon={ShoppingBagIcon}>Berdasarkan Penjualan</Tab>
--- app/stock-opnames/tab-list.tsx
+++ app/stock-opnames/tab-list.tsx
@@ -1,10 +1,17 @@
 "use client"
 
+import { usePrintMode } from "@/lib/print-mode"
 import { BookOpenIcon, CircleStackIcon } from "@heroicons/react/24/outline"
 import { Tab, TabList } from "@tremor/react"
 
 // StockOpnameTabList is created in a different file because it needs to be a Client Component to import the icons, while we want the other parts to be Server Components.
 export default function StockOpnameTabList(): React.ReactElement {
+    const { isPrintMode } = usePrintMode()
+
+    if (isPrintMode) {
+        return <></>
+    }
+
     return (
         <TabList>
             <Tab icon={BookOpenIcon}>Ringkasan</Tab>
