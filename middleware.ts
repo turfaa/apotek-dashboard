5281a60eaf694e8002633ff14bb6bc428e14f200
Turfa Auliarachman <me@turfa.id>
2024-02-13 05:52:25 +0700

    fix: Add authorization on API rewrites

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,31 @@
-import { NextResponse } from "next/server"
-import { NextRequestWithAuth, withAuth } from "next-auth/middleware"
-import { isPageAllowed } from "@/lib/navigation"
 import { Role } from "@/lib/api/auth"
+import { isPageAllowed } from "@/lib/navigation"
+import { NextRequestWithAuth, withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+interface RewriteRule {
+    source: string
+    destination: string
+    allowedRoles: Role[]
+}
+
+const rewrites = [
+    {
+        source: "/api/drugs",
+        destination: `${process.env.VMEDIS_PROXY_URL}/drugs`,
+        allowedRoles: [Role.ADMIN, Role.STAFF, Role.RESELLER, Role.GUEST],
+    },
+    {
+        source: "/api/sales/statistics",
+        destination: `${process.env.VMEDIS_PROXY_URL}/sales/statistics`,
+        allowedRoles: [Role.ADMIN],
+    },
+    {
+        source: "/api/procurements/recommendations",
+        destination: `${process.env.VMEDIS_PROXY_URL}/procurements/recommendations`,
+        allowedRoles: [Role.ADMIN, Role.STAFF],
+    },
+] satisfies RewriteRule[]
 
 export default withAuth(
     async function middleware(
@@ -10,10 +34,18 @@ export default withAuth(
         const { pathname, search } = request.nextUrl
         const { token } = request.nextauth
 
-        if (pathname.includes("/api") && !pathname.includes("/api/auth")) {
-            const path = pathname.replace("/api", "")
-            const url = `${process.env.VMEDIS_PROXY_URL}${path}${search}`
-            return NextResponse.rewrite(url)
+        for (const rule of rewrites) {
+            if (pathname.includes(rule.source)) {
+                if (!rule.allowedRoles.includes(token?.role as Role)) {
+                    return NextResponse.json(
+                        { message: "Unauthorized" },
+                        { status: 401 },
+                    )
+                }
+
+                const url = `${rule.destination}${search}`
+                return NextResponse.rewrite(url)
+            }
         }
 
         const firstPathSegment = `/${pathname.split("/")[1]}`
