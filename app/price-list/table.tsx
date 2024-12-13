712fa0077a894274ae97216adc8d5a45510e83e2
Turfa Auliarachman <me@turfa.id>
2024-12-14 01:30:41 +0700

    fix: fix PriceListTable

--- app/price-list/table.tsx
+++ app/price-list/table.tsx
@@ -1,13 +1,12 @@
 import { getDrugs } from "@/lib/api/drugv2"
 import PriceListTableClient from "./table-client"
 import { auth } from "@/lib/auth"
-import { use } from "react"
 
 export default function PriceListTable(): React.ReactElement {
-    const session = use(auth())
-    const { drugs } = use(getDrugs(session))
+    const sessionPromise = auth()
+    const drugsPromise = sessionPromise.then((session) => getDrugs(session)).then((drugs) => drugs.drugs)
 
     return (
-        <PriceListTableClient session={session} initialDrugs={drugs} />
+        <PriceListTableClient sessionPromise={sessionPromise} initialDrugsPromise={drugsPromise} />
     )
 }
